5a5a7da91ace295675098de75cd4e78f671069a9
Viet Le Nguyen Ngoc <viet.lenguyenngoc@emea.nttdata.com>
2023-08-18 17:58:45 +0700

    initial commit

---
+++ src/main/java/com/vietlnn/serverless/java/services/ServerlessJavaServicesApplication.java
@@ -0,0 +1,13 @@
+package com.vietlnn.serverless.java.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerlessJavaServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerlessJavaServicesApplication.class, args);
+	}
+
+}
