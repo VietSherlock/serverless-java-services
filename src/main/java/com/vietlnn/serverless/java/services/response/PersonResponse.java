3e0dc6d7e354d6f4a60ed8c49fe3ae0958ec5be2
VietSherlock <vieetsherlock@gmail.com>
2023-08-19 09:33:12 +0700

    add creating person logic

---
+++ src/main/java/com/vietlnn/serverless/java/services/response/PersonResponse.java
@@ -0,0 +1,13 @@
+package com.vietlnn.serverless.java.services.response;
+
+public class PersonResponse {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
